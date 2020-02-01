e3950cee4f06f7b74d9b7c90361b73e149d75c07
Cesar Castro <cesarcastro15@gmail.com>
2020-02-01 17:48:16 +0100

    Added dialog event and logic

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -7,18 +7,47 @@ public class GameManager : MonoBehaviour
     public delegate void GameManagerEventHandler();
 
     public event GameManagerEventHandler ExitGameEvent;
+    //Game Events
+    public event GameManagerEventHandler ShowDialogsEvent; //shows all dialogs in the screen
+    public event GameManagerEventHandler ScrollEvent;//simulates the scrolling in the timeline
+    public event GameManagerEventHandler ShowKeysEvent;//shows all the keys available for the current dot
+    public event GameManagerEventHandler ChangeOptionEvent;//changes another option
+    public event GameManagerEventHandler AdquireKeyEvent;//when the user gets to a sentence that has a key
         
-    private ConversationManager conversationManager;
+    public ConversationManager conversationManager { get; set; }
 
 
     void Start()
     {
-        conversationManager = new ConversationManager();   
+        conversationManager = new ConversationManager();
+        CallShowDialogsEvent();
     }
 
     public void CallExitGameEvent()
     {
         ExitGameEvent?.Invoke();
     }
+    public void CallShowDialogsEvent()
+    {
+        ShowDialogsEvent?.Invoke();
+    }
+    public void CallScrollEvent() 
+    {
+        ScrollEvent?.Invoke();
+    }
+    public void CallShowKeysEvent()
+    {
+        ShowKeysEvent?.Invoke();
+    }
+    public void CallChangeOptionsEvent()
+    {
+        ChangeOptionEvent?.Invoke();
+    }
+    public void CallAdquireKeyEvent() 
+    {
+        AdquireKeyEvent?.Invoke();
+    }
 
+
+        
 }
