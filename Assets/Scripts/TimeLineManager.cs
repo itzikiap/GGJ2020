35e003e77f386132aa3b8acfa28faefe661d8714
Cesar Castro <cesarcastro15@gmail.com>
2020-02-01 16:05:29 +0100

    TimeLine base added

---
+++ Assets/Scripts/TimeLineManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLineManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject dot;
+    [SerializeField]
+    private GameObject line;
+
+    public List<GameObject> dots = new List<GameObject>();
+
+    private void Start()
+    {
+
+    }
+    public void addDot(int x)
+    {
+
+        var dotInitiliazed = Instantiate(dot, Vector3.zero, Quaternion.identity);
+        dotInitiliazed.transform.SetParent(line.transform);
+        dotInitiliazed.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+        dotInitiliazed.GetComponent<RectTransform>().localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        
+    }
+    
+}
