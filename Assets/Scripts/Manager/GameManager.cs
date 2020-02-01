65b0af3c81d76425b925aa66080faebc64db5235
Cesar Castro <cesarcastro15@gmail.com>
2020-02-01 13:17:58 +0100

    Added Game Manager

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameManagerEventHandler();
+    // Start is called before the first frame update
+    private ConversationManager conversationManager;
+    void Start()
+    {
+        conversationManager = new ConversationManager();   
+    }
+
+}
